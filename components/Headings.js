4a98cf351e19f6eec5dc5ac9d692497a671f6578
Stormblessed <carlomanid@gmail.com>
2023-08-23 18:08:14 +0300

    Add new heading

--- components/Headings.js
+++ components/Headings.js
@@ -2,30 +2,36 @@
 
 const { styled, css } = require("styled-components");
 
+const bar = css`
+    &::after {     
+        content: "";
+        position: absolute;
+        bottom: -1rem;
+        left: 0px;
+        background-color: var(--color-grey-2);
+        width: 100px;
+        height: 5px;
+        border-radius: 2px;
+    }
+`
+
 const variations = {
     primary: css`
         background-color: var(--color-grey-1);
         font-size: 4rem;
     `,
+    secondary: css`
+        font-size: 2.8rem;
+    `,
     sub: css`
         color: var(--color-grey-2);
-        position: relative;
-
-        &::after {     
-            content: "";
-            position: absolute;
-            bottom: -1rem;
-            left: 0px;
-            background-color: var(--color-grey-2);
-            width: 100px;
-            height: 5px;
-            border-radius: 2px;
-        }
     `,
 };
 
 const Heading = styled.h2`
-      ${props => variations[props.variation]};
+    position: relative;
+    ${props => variations[props.variation]};
+    ${props => props.bar && bar};
 `;
 
 export default Heading;
