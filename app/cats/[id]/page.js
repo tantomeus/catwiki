47cabe75f0d9343fd46cf049e96b01fb0ccabf0f
Stormblessed <carlomanid@gmail.com>
2023-08-27 18:59:49 +0300

    Change react-query to server loader

--- app/cats/[id]/page.js
+++ app/cats/[id]/page.js
@@ -1,87 +1,9 @@
-"use client";
-
-import Characteristics from "@/components/Characteristics";
-import Heading from "@/components/Headings";
+import CatInfo from "@/components/CatInfo";
 import { getACat } from "@/services/apiCats";
-import { useQuery } from "@tanstack/react-query";
-import Image from "next/image";
-import { useParams } from "next/navigation";
-import { styled } from "styled-components";
 
-const Container = styled.div`
-    display: grid;
-    grid-template-columns: 350px 1fr;
-    column-gap: 4rem;
-`;
+export default async function Page({ params }) {
+    const { id } = params;
+    const data = await getACat(id);
 
-const ImageWrapper = styled.div`
-    justify-self: center;
-    align-self: center;
-`;
-
-const Info = styled.div`
-    background-color: var(--color-grey-1);
-    padding: 5.4rem 3.6rem 3.6rem 18rem;
-    border-radius: 270px 36px 0px 0px;
-    letter-spacing: 1.5px;
-
-    display: flex;
-    flex-direction: column;
-    gap: 2rem;
-    font-size: 1.8rem;
-
-    & p {
-        color: rgb(120, 113, 108);
-    }
-
-    & span {
-        color: black;
-        font-weight: 700;
-    }
-`;
-
-const GalleryWrapper = styled.div`
-    grid-column: 1/-1;
-
-    padding: 5.4rem 3rem;
-    background-color: var(--color-grey-1);
-`;
-
-const Gallery = styled.div`
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    justify-items: center;
-    align-items: center;
-    gap: 3.6rem;
-    margin-top: 4.5rem;
-`;
-
-export default function Page() {
-    const { id } = useParams();
-    const { isLoading, data, error } = useQuery({
-        queryKey: [],
-        queryFn: () => getACat(id)
-    });
-
-    const myLoader=({src})=>{
-        return `${src}?w=${265}`;
-    }
-
-    return <Container>
-        <ImageWrapper><Image style={{objectFit: "cover", borderRadius: "18px"}} loader={myLoader} width={350} height={350} src={data?.imgs[0]?.url} alt="cat"/></ImageWrapper>
-        <Info>
-            <Heading as="h1" variation="primary">{data?.name}</Heading>
-            <p>{data?.description}</p>
-            <p><span>Temperament:</span> {data?.temperament}</p>
-            <p><span>Origin:</span> {data?.origin}</p>
-            <p><span>Lifespan:</span> {data?.life_span} years</p>
-        </Info>
-        <Characteristics data={data} />
-        <GalleryWrapper>
-            <Heading variation="secondary" bar="barMd">Other Photos</Heading>
-            <Gallery>
-            {data?.imgs.slice(1).map( (img, i) => <Image key={i} style={{objectFit: "cover", borderRadius: "18px"}} loader={myLoader} width={265} height={350} src={img.url} alt="cat"/>)}
-            </Gallery>
-        </GalleryWrapper>
-    </Container>;
+    return <CatInfo data={data} />
 }
